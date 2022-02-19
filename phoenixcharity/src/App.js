d89244422492d422f680aac8d8d1955e99dc8071
krishnakant01 <63720159+krishnakant01@users.noreply.github.com>
2022-02-19 19:05:30 +0530

    img added

--- phoenixcharity/src/App.js
+++ phoenixcharity/src/App.js
@@ -7,6 +7,16 @@ function App() {
   return (
     <div className="App">
       <Navigation/>
+
+      <div class="grid-50-50-layout">
+        <div class="image container">
+          <img src={homeImg} alt="child"></img>
+        </div>
+
+        <div>
+
+        </div>
+      </div>
     </div>
   );
 }
